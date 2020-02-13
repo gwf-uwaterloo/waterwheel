03d5d4070701d8d8f61aa52d1779d6947c942ccb
Govind Sharma <govind9@cedar5.cedar.computecanada.ca>
2020-02-13 10:07:21 -0800

    Filter rivers with length property

--- scripts/download_wikidata_rivers.py
+++ scripts/download_wikidata_rivers.py
@@ -4,10 +4,11 @@ import json
 
 url = 'https://query.wikidata.org/sparql'
 subquery = """
-SELECT ?item ?itemLabel 
+SELECT ?item ?itemLabel ?length
 WHERE 
 {
   ?item wdt:P31 wd:Q4022.
+  ?item wdt:P2043 ?length
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
   FILTER (CONTAINS(STR(?item), 
 """
@@ -20,19 +21,18 @@ for comb in tqdm(combinations):
         data = r.json()
         for entry in data["results"]["bindings"]:
             key = entry["item"]["value"].split("/")[-1]
-            if key in rivers:
-                print ("haye oyee rabba")
-            rivers[key] = entry['itemLabel']['value']
+            rivers[key] = (entry['itemLabel']['value'], entry['length']['value'])
     except Exception as e:
         print (str(e))
         print (comb)
 
+print ("length of rivers", len(rivers))
 with open("wikidata_rivers.json", "w") as file:
     file.write(json.dumps(rivers))
 
-csv = "ItemLabel,Item\n"
+csv = "ID,Name,Length\n"
 for key, value in rivers.items():
-    csv += f"{value.replace(',', ';')},{key.replace(',', ';')}\n"
+    csv += f"{value[0].replace(',', ';')},{key.replace(',', ';')},{value[1]}\n"
 
 with open("wikidata_rivers.csv", "w") as file:
     file.write(csv)
