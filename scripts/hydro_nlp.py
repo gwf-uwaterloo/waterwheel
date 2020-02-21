60d92cae0627aa82565d742a3592d5d97eb52595
dnivoG amrahS <dnivogamrahs@gmail.com>
2020-02-21 02:18:43 -0500

    Add extra attribute: wikilinks for matches. Method documentation

--- scripts/hydro_nlp.py
+++ scripts/hydro_nlp.py
@@ -1,68 +1,141 @@
 import spacy
 from spacy.matcher import PhraseMatcher
+from spacy.tokens import Doc, Span, Token
 
 import pickle
 import re
 from tqdm import tqdm
 
 class HydroMatch():
-    
-    def __init__(self, matcher_file = "hydro_matcher.pkl"):
+    """Class for NLP related to Hydrology"""
+    def __init__(self, matcher_file: str = "hydro_matcher.pkl"):
+        """Initialize the class.
+        
+        Parameters
+        ----------
+        matcher_file : str, optional
+            File path to load the vocab and wikidata.
+            File created using HydroMatch::save_vocab method should be used for this.
+        """
+        
         self.matcher = None
         self.nlp = spacy.load("en_core_web_sm")
         self.vocab = {}
+        self.wikidata = {}
         try:
             with open(matcher_file, "rb") as file:
-                self.vocab, self.matcher = pickle.load(file)
+                self.vocab, self.matcher, self.wikidata = pickle.load(file)
             print (f"Loaded water bodies from {matcher_file}")
         except Exception as e:
             print (str(e))
             print (f"Failed to load water bodies from {matcher_file}")
+        Span.set_extension("wikilink", default = None)
     
-    def load_vocab(self, water_bodies, append = True):
+    def load_vocab(self, water_bodies: Dict, append: bool = True):
+        """Load new vocab and wikidata.
+        
+        Parameters
+        ----------
+        water_bodies : Dict
+            Dictionary containing the list of new water bodies to be loaded.
+            Format:
+            {
+                "LAKE": [(Name, Wiki_Id), ...],
+                "RIVER": [(Name, Wiki_Id), ...],
+                ...
+            }
+        append : bool, optional
+            If True then the new data is appended on top of the previous data.
+            Otherwise old data is done away with.
+        """
         if append == False or self.matcher is None:
             self.matcher = PhraseMatcher(self.nlp.vocab, attr = 'LOWER')
             self.vocab = {}
+            self.wikidata = {}
         for key in water_bodies:
             print (f"Loading {key}")
-            phrases = [self.nlp(wb) for wb in tqdm(water_bodies[key])]
+            phrases = [self.nlp(wb) for wb, _ in tqdm(water_bodies[key])]
             self.vocab[key] = self.nlp.vocab.strings[key]
             self.matcher.add(key.upper(), None, *phrases)
+            if key not in self.wikidata:
+                self.wikidata[key] = {}
+            for name, id in water_bodies[key]:
+                self.wikidata[key][name.lower()] = id
     
     def save_vocab(self, filename = "hydro_matcher.pkl"):
+        """Save vocab and wikidata to file.
+        
+        Parameters
+        ----------
+        filename : str, optional
+            Path to file to save the vocab and wikidata to.
+        """
         try:
             with open(filename, "wb") as file:
-                pickle.dump((self.vocab, self.matcher), file)
+                pickle.dump((self.vocab, self.matcher, self.wikidata), file)
             print (f"Vocab stored in {filename}")
         except Exception as e:
             print (str(e))
             print (f"Failed to store vocab in {filename}")
     
-    def match_type(self, hash):
+    def match_type(self, hash: int):
+        """Determine the type for the water body.
+        
+        Parameters
+        ----------
+        hash : int
+            The hash of the string representing each type
+        
+        Returns
+        -------
+        type: str
+            Type of the water body eg LAKE, RIVER
+        """
         for key in self.vocab:
             if self.vocab[key] == hash:
                 return key
         return "WATER_BODY"
     
-    def match(self, text):
+    def match(self, text: str):
+        """Match given text against vocab.
+        
+        Parameters
+        ----------
+        text : str
+            Text to search for water bodies.
+        
+        Returns
+        -------
+        final_matches: List
+            List of matches found in the text.
+            Format:
+                [(match_word, start, end), ...]
+        doc: Doc
+            spaCy Doc object containing entities with matches.
+            To be rendered as:
+                displacy.render(doc, style="ent", jupyter=True)
+            Get list of extra attributes for each entity/match:
+                 [ent._.wikilink for ent in doc.ents]
+        """
         if self.matcher is None:
             raise AttributeError("Model is not set.")
         
         doc = self.nlp(text)
-        matches = [(str(doc[match[1]:match[2]]), self.match_type(match[0])) 
+        matches = [(str(doc[match[1]:match[2]]), self.match_type(match[0]), match[1], match[2]) 
                          for match in self.matcher(doc)]
        
-        entities = [{
-            "text": text,
-            "ents": []
-        }]
+        entities = []
         final_matches = []
         for match in matches:
             if re.search("^[\sA-Z]+$", match[0]) or re.search("^[\sa-z]+$", match[0]):
                 continue
             final_matches.append(match)
-            for m in re.finditer(match[0], text):
-                entities[0]["ents"].append({
-                    "start": m.start(), "end": m.end(), "label": match[1], "title": None
-                })
-        return final_matches, entities
+            entity = Span(doc, match[2], match[3], label=match[1])
+            if match[1] in self.wikidata and match[0].lower() in self.wikidata[match[1]]:
+                entity._.set(
+                    "wikilink", 
+                    "https://www.wikidata.org/wiki/" + self.wikidata[match[1]][match[0].lower()]
+                )
+            entities.append(entity)
+        doc.ents = entities
+        return final_matches, doc
---
+++ scripts/pickle_wikidata.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import re
+import pickle
+import spacy
+from spacy.matcher import PhraseMatcher
+from spacy.tokens import Doc, Span, Token
+
+import pickle
+import re
+from tqdm import tqdm
+
+L = pd.read_csv("H:\\Courses\\RA\\wikidata-water-features\\data\\wikidata_lakes.csv")
+R = pd.read_csv("H:\\Courses\\RA\\wikidata-water-features\\data\\wikidata_rivers.csv")
+wikidata = {"LAKE": {}, "RIVER": {}}
+
+for i in range(len(L)):
+    if not re.search("^Q[0-9]+", L["Name"][i]):
+        wikidata["LAKE"][L["Name"][i].lower()] = L["ID"][i]
+for i in range(len(R)):
+    if not re.search("^Q[0-9]+", R["Name"][i]):
+        wikidata["RIVER"][R["Name"][i].lower()] = R["ID"][i]
+
+with open("H:\\Courses\\RA\\wikidata-water-features\\scripts\\hydro_matcher.pkl", "rb") as file:
+    vocab, matcher = pickle.load(file)
+
+with open("H:\\Courses\\RA\\wikidata-water-features\\scripts\\hydro_matcher_new.pkl", "wb") as file:
+    pickle.dump((vocab, matcher, wikidata), file)
+
+
+with open("H:\\Courses\\RA\\wikidata-water-features\\scripts\\hydro_matcher_new.pkl", "rb") as file:
+    vocab, matcher, wikidata = pickle.load(file)
