5adb3f1f8ce0d646e47b09c7156a43fd9328a5e8
dnivoG amrahS <dnivogamrahs@gmail.com>
2020-03-07 06:25:02 -0500

    Turn into spacy pipeline component

--- scripts/util.py
+++ scripts/util.py
@@ -1,28 +1,22 @@
 import re
 import os
-import pickle
-import json
+import srsly
 import pandas as pd
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, Tuple
 from tqdm import tqdm
+from collections import OrderedDict
 
-from spacy.tokens import Doc, Span, DocBin
-from spacy.matcher import PhraseMatcher
-from spacy import language
+from spacy.tokens import DocBin
+from spacy.language import Language
 
-doc_bin_file = Path(os.path.dirname(os.path.realpath(__file__))) / 'resources/doc_bin.pkl'
-vocab_file = Path(os.path.dirname(os.path.realpath(__file__))) / 'resources/vocab.json'
-wikidata_file = Path(os.path.dirname(os.path.realpath(__file__))) / 'resources/wikidata.json'
-stop_words_file = Path(os.path.dirname(os.path.realpath(__file__))) / 'resources/stop_words.txt'
+doc_bins_file = Path(os.path.dirname(os.path.realpath(__file__))) / 'data/doc_bins.msgpack'
+vocab_file = Path(os.path.dirname(os.path.realpath(__file__))) / 'data/vocab.json'
+wikidata_file = Path(os.path.dirname(os.path.realpath(__file__))) / 'data/wikidata.json'
+stop_words_file = Path(os.path.dirname(os.path.realpath(__file__))) / 'data/stop_words.json'
 
-def read_files(nlp: language):
-    """Load necessary data from resource files.
-    
-    Parameters
-    ----------
-    nlp: language
-        spacy nlp object
+def read_data_files():
+    """Load necessary data from data files.
     
     Returns
     -------
@@ -30,50 +24,16 @@ def read_files(nlp: language):
         A dictionary containing the different water body types and their hash value.
     wikidata: Dict
         A dictionary with each wikilink data for every water_body
-    stop_words: List
+    stop_words: set
         A set of common words in English
-    matcher: PhraseMatcher
-        A PhraseMatcher object with all vocab loaded.
     """
-    
-    vocab = {}
-    wikidata = {}
-    stop_words = set()
-    matcher = None
-    
-    try:
-        with open(vocab_file) as file:
-            vocab = json.load(file)
-    except Exception as e:
-        raise RuntimeError(f'Failed to load vocab from {vocab_file}.\nError: {str(e)}')
 
-    try:
-        with open(wikidata_file) as file:
-            wikidata = json.load(file)
-    except Exception as e:
-        raise RuntimeError(f'Failed to load wikidata from {wikidata_file}.\nError: {str(e)}')
+    vocab = srsly.read_json(vocab_file)
+    wikidata = srsly.read_json(wikidata_file)
+    stop_words = set(srsly.read_json(stop_words_file)['stop_words'])
+    return vocab, wikidata, stop_words
 
-    try:
-        with open(stop_words_file) as file:
-            stop_words = set(file.read().splitlines())
-    except Exception as e:
-        raise RuntimeError(f'Failed to load stop_words from {stop_words_file}.\nError: {str(e)}')
-
-    try:
-        with open(doc_bin_file, 'rb') as file:
-            doc_bins_bytes = pickle.load(file)
-        doc_bins = {key: DocBin().from_bytes(value) for key, value in doc_bins_bytes.items()}
-        phrases_bin = {key: list(bin.get_docs(nlp.vocab)) for key, bin in doc_bins.items()}
-        matcher = PhraseMatcher(nlp.vocab, attr='LOWER')
-        for key, phrases in phrases_bin.items():
-            matcher.add(key.upper(), None, *phrases)
-            #matcher.add(key.upper(), phrases)
-    except Exception as e:
-        raise RuntimeError(f'Failed to load water bodies from {doc_bin_file_file}.\nError: {str(e)}')
-    
-    return vocab, wikidata, stop_words, matcher
-
-def write_files(vocab, wikidata, phrase_map):
+def write_data_files(vocab: Dict, wikidata: Dict, stop_words: set, doc_bins_bytes: Dict):
     """Writes necessary data to resource files.
     
     Parameters
@@ -82,35 +42,44 @@ def write_files(vocab, wikidata, phrase_map):
         A dictionary containing the different water body types and their hash value.
     wikidata: Dict
         A dictionary with each wikilink data for every water_body
-    phrase_map: Dict
-        A map of different water body types and DocBin of phrases.
+    stop_words: set
+        A set of commong words in English.
+    doc_bins_bytes: Dict
+        A dictionary of DocBin bytes for each water body type.
     """
-    
-    try:
-        with open(vocab_file, 'w') as file:
-            json.dump(vocab, file)
-    except Exception as e:
-        raise RuntimeError(f'Failed to write vocab to {vocab_file}.\nError: {str(e)}')
-    
-    try:
-        with open(wikidata_file, 'w') as file:
-            json.dump(wikidata, file)
-    except Exception as e:
-        raise RuntimeError(f'Failed to write wikidata to {wikidata_file}.\nError: {str(e)}')
-    
-    try:
-        doc_bins = {}
-        for key in phrase_map:
-            doc_bin = DocBin()
-            for doc in phrase_map[key]:
-                doc_bin.add(doc)
-            doc_bins[key] = doc_bin.to_bytes()
-        with open(doc_bin_file, 'wb') as file:
-            pickle.dump(doc_bins, file)
-    except Exception as e:
-        raise RuntimeError(f'Failed to write water_bodies to {doc_bin_file}.\nError: {str(e)}')       
 
-def add_vocab(water_bodies: Dict, nlp, append: bool = True):
+    serial = OrderedDict(
+        (
+            ('stop_words', list(stop_words)),
+            ('vocab', vocab),
+            ('wikidata', wikidata),
+            ('doc_bins', doc_bins_bytes),
+        )
+    )
+    srsly.write_msgpack(doc_bins_file, serial)
+
+def name_split(name: str):
+    """Function to split waterbody names.
+
+    Parameters
+    ----------
+    name : str
+        name of the waterbody eg 'amazon riverbasin'
+    
+    Returns
+    -------
+    names : Tuple
+        all possible variations of name 
+        eg ('amazon riverbasin', 'amazon')
+    """
+
+    s = name.lower()
+    tokens = ['river', 'lake', 'basin']
+    for token in tokens:
+        s = s.replace(token, "")
+    return (name, s.strip())
+
+def build_vocab(water_bodies: Dict, nlp: Language):
     """Load new vocab and wikidata.
     
     Parameters
@@ -123,35 +92,30 @@ def add_vocab(water_bodies: Dict, nlp, append: bool = True):
             "RIVER": [(Name, Wiki_Id), ...],
             ...
         }
-    nlp:
+    nlp: Language
         spacy nlp object
-    append : bool, optional
-        If True then the new data is appended on top of the previous data.
-        Otherwise old data is done away with.
     """
-    
-    if append:
-        vocab, wikidata, stop_words, matcher = read_files(nlp)
-    else:
-        matcher = PhraseMatcher(nlp.vocab, attr='LOWER')
-        vocab = {}
-        wikidata = {}
-    
-    phrase_map = {}
+
+    vocab = {}
+    wikidata = {}
+    doc_bins_bytes = {}
+    stop_words = set(srsly.read_json(stop_words_file)['stop_words'])
+
     for key in water_bodies:
-        phrases = [nlp(wb) for wb, _ in tqdm(water_bodies[key], desc=f'Loading {key}(s)')]
-        phrase_map[key] = phrases
-        vocab[key] = nlp.vocab.strings[key]
-        matcher.add(key.upper(), None, *phrases)
-        #matcher.add(key.upper(), phrases)
+        doc_bin = DocBin()
+        for wb, _ in tqdm(water_bodies[key], desc=f'Loading {key}(s)'):
+            doc_bin.add(nlp(wb))
+        doc_bins_bytes[key] = doc_bin.to_bytes()
+        
         if key not in wikidata:
             wikidata[key] = {}
         for name, id in water_bodies[key]:
             wikidata[key][name.lower()] = id
-    
-    write_files(vocab, wikidata, phrase_map)
+        
+        vocab[str(nlp.vocab.strings[key])] = key
+    write_data_files(vocab, wikidata, stop_words, doc_bins_bytes)
 
-def add_vocab_csvs(data_dir: Path, nlp, append: bool = True):
+def build_vocab_csvs(data_dir: Path, nlp: Language):
     """Load data from csv files.
     
     Parameters
@@ -162,11 +126,8 @@ def add_vocab_csvs(data_dir: Path, nlp, append: bool = True):
             Each csv file should contain columns Name and ID
             Filename should be wikidata_{water_body_type}s.csv
             For example wikidata_rivers.csv
-    nlp:
+    nlp : Language
         spacy nlp object
-    append : bool, optional
-        If True then the new data is appended on top of the previous data.
-        Otherwise old data is done away with.
     """
     
     water_bodies = {}
@@ -175,16 +136,14 @@ def add_vocab_csvs(data_dir: Path, nlp, append: bool = True):
     files = [str(f) for f in files]
     
     for file in files:
-        try:
-            wb_type = file.split('wikidata_')[1].split('s.csv')[0].upper()
-            water_bodies[wb_type] = []
-            df = pd.read_csv(file)
-            for i in range(len(df)):
-                if (type(df['Name'][i]) is str and
-                        len(df['Name'][i]) > 1 and
-                        (not re.search('^Q[0-9]+', df['Name'][i]))):
-                    water_bodies[wb_type].append((df['Name'][i].lower(), df['ID'][i]))
-        except Exception as e:
-            raise RuntimeError(f'Could not load {file}.\nError: {str(e)}')
-    
-    add_vocab(water_bodies, nlp, append=append)
+        wb_type = file.split('wikidata_')[1].split('s.csv')[0].upper()
+        water_bodies[wb_type] = []
+        df = pd.read_csv(file)
+        for i in range(len(df)):
+            if (type(df['Name'][i]) is str and
+            len(df['Name'][i]) > 1 and
+            (not re.search('^Q[0-9]+', df['Name'][i]))):
+                names = name_split(df['Name'][i])
+                # split 'amazon river' or 'river amazon' to 'amazon'
+                for name in names: water_bodies[wb_type].append((name, df['ID'][i]))
+    build_vocab(water_bodies, nlp)
