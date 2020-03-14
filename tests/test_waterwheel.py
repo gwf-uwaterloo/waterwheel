aa7569815fb6a2eee8edc7db9ef709db0c05012b
dnivoG amrahS <dnivogamrahs@gmail.com>
2020-03-14 04:44:31 -0400

    Added precedence rules and test cases.

--- tests/test_waterwheel.py
+++ tests/test_waterwheel.py
@@ -11,6 +11,45 @@ class TestWaterWheel(unittest.TestCase):
     
     def test_pipeline_added(self):
         self.assertEqual(self.nlp.pipe_names[-1], 'waterwheel')
+
+    def test_proper_nouns(self):
+        text = 'The amazon is something and the AMAZON is something.'
+        doc = self.nlp(text)
+        self.assertEqual(len(doc.ents), 0)
+
+        text = 'The Amazon is something.'
+        doc = self.nlp(text)
+        self.assertEqual(str(doc.ents[0]), 'Amazon')
+        self.assertEqual(str(doc.ents[0].label_), 'RIVER')
+        for ent in doc.ents:
+            self.assertIsNotNone(ent._.wikilink)
+
+        text = 'The amazon river is something and the AMAZON river is something'
+        doc = self.nlp(text)
+        self.assertEqual(str(doc.ents[0]), 'amazon river')
+        self.assertEqual(str(doc.ents[0].label_), 'RIVER')
+        self.assertEqual(str(doc.ents[1]), 'AMAZON river')
+        self.assertEqual(str(doc.ents[1].label_), 'RIVER')
+        for ent in doc.ents:
+            self.assertIsNotNone(ent._.wikilink)
+        
+    def test_common_uncommon_overlap(self):
+        text = 'The River Cherwell is a major tributary of the River Thames in central England.'
+        doc = self.nlp(text)
+        self.assertEqual(str(doc.ents[0]), 'River Cherwell')
+        self.assertEqual(str(doc.ents[0].label_), 'RIVER')
+        self.assertEqual(str(doc.ents[1]), 'River Thames')
+        self.assertEqual(str(doc.ents[1].label_), 'RIVER')
+        for ent in doc.ents:
+            self.assertIsNotNone(ent._.wikilink)
+        text = 'The Lake Ontario is surely awesome and so is river Thames.'
+        doc = self.nlp(text)
+        self.assertEqual(str(doc.ents[0]), 'Lake Ontario')
+        self.assertEqual(str(doc.ents[0].label_), 'LAKE')
+        self.assertEqual(str(doc.ents[1]), 'river Thames')
+        self.assertEqual(str(doc.ents[1].label_), 'RIVER')
+        for ent in doc.ents:
+            self.assertIsNotNone(ent._.wikilink)
     
     def test_compound_references(self):
         text = '''Aggregated gridded soil texture dataset 
@@ -26,7 +65,7 @@ class TestWaterWheel(unittest.TestCase):
         self.assertEqual(str(doc.ents[2].label_), 'RIVER')
         self.assertEqual(str(doc.ents[3]), 'Nelson')
         self.assertEqual(str(doc.ents[3].label_), 'RIVER')
-        self.assertEqual(str(doc.ents[4]), 'Churchill')
+        self.assertEqual(str(doc.ents[4]), 'Churchill River')
         self.assertEqual(str(doc.ents[4].label_), 'RIVER')
 
         for ent in doc.ents:
@@ -41,32 +80,66 @@ class TestWaterWheel(unittest.TestCase):
         doc = self.nlp(text)
 
         self.assertEqual(str(doc.ents[0]), 'Saint Lawrence')
+        self.assertEqual(str(doc.ents[0].label_), 'RIVER')
         self.assertEqual(str(doc.ents[1]), 'Ontario')
+        self.assertEqual(str(doc.ents[1].label_), 'LAKE')
         self.assertEqual(str(doc.ents[2]), 'Erie')
-        self.assertEqual(str(doc.ents[3]), 'Saint Lawrence')
+        self.assertEqual(str(doc.ents[2].label_), 'LAKE')
+        self.assertEqual(str(doc.ents[3]), 'Saint Lawrence River')
+        self.assertEqual(str(doc.ents[3].label_), 'RIVER')
         self.assertEqual(str(doc.ents[4]), 'Mississippi')
+        self.assertEqual(str(doc.ents[4].label_), 'RIVER')
         self.assertEqual(str(doc.ents[5]), 'Missouri')
+        self.assertEqual(str(doc.ents[5].label_), 'RIVER')
 
+        for ent in doc.ents:
+            self.assertIsNotNone(ent._.wikilink)
+    
+    def test_qualifier_effect(self):
+        text = 'Is Mississippi a river or a lake?'
+        doc = self.nlp(text)
+        self.assertEqual(str(doc.ents[0]), 'Mississippi')
+        self.assertEqual(str(doc.ents[0].label_), 'RIVER')
+        for ent in doc.ents:
+            self.assertIsNotNone(ent._.wikilink)
+        text = 'Is Mississippi Lake a river or a lake?'
+        doc = self.nlp(text)
+        self.assertEqual(str(doc.ents[0]), 'Mississippi Lake')
+        self.assertEqual(str(doc.ents[0].label_), 'LAKE')
+        for ent in doc.ents:
+            self.assertIsNotNone(ent._.wikilink)
+
+    def test_left_priority(self):
+        text = 'Is Great Slave Lake Ontario related?'
+        doc = self.nlp(text)
+        self.assertEqual(str(doc.ents[0]), 'Great Slave Lake')
+        self.assertEqual(str(doc.ents[0].label_), 'LAKE')
+        self.assertEqual(len(doc.ents), 1)
         for ent in doc.ents:
             self.assertIsNotNone(ent._.wikilink)
 
     def test_common_names(self):
         doc = self.nlp('Is Nile an actual river?')
         self.assertNotEqual(str(doc.ents[0]), 'Is')
+        for ent in doc.ents:
+            self.assertIsNotNone(ent._.wikilink)
 
         doc = self.nlp('There is an actual river named Is river or is river or IS river.')
-        self.assertEqual(str(doc.ents[0]), 'Is')
-        self.assertEqual(str(doc.ents[1]), 'is')
-        self.assertEqual(str(doc.ents[2]), 'IS')
+        self.assertEqual(str(doc.ents[0]), 'Is river')
+        self.assertEqual(str(doc.ents[0].label_), 'RIVER')
+        self.assertEqual(str(doc.ents[1]), 'is river')
+        self.assertEqual(str(doc.ents[1].label_), 'RIVER')
+        self.assertEqual(str(doc.ents[2]), 'IS river')
+        self.assertEqual(str(doc.ents[2].label_), 'RIVER')
         for ent in doc.ents:
             self.assertIsNotNone(ent._.wikilink)
     
     def test_issue20(self):
         doc = self.nlp('The Mackenzie River flows from the Great Slave Lake into the Arctic Ocean.')
-        self.assertEqual(str(doc.ents[1]), 'Great Slave')
+        self.assertEqual(str(doc.ents[1]), 'Great Slave Lake')
         self.assertEqual(str(doc.ents[1].label_), 'LAKE')
         doc = self.nlp('The Mackenzie River flows from the great slave lake into the Arctic Ocean.')
-        self.assertEqual(str(doc.ents[1]), 'great slave')
+        self.assertEqual(str(doc.ents[1]), 'great slave lake')
         self.assertEqual(str(doc.ents[1].label_), 'LAKE')
 
         for ent in doc.ents:
